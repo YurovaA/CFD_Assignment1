8d8efdecb81b4ab21dadb411e6c424428aac6f6d
SaumitraVinayJoshi <saumitra.vjoshi@gmail.com>
2015-04-27 00:12:20 +0200

    Changed array notation, merged for-loops

--- boundary_val.c
+++ boundary_val.c
@@ -7,32 +7,26 @@ void boundaryvalues(
   double **V
 ){
     int i, j;
-    int imax_plus_two = imax + 2;
     int imax_plus_one = imax + 1;
-    //int jmax_plus_two = jmax + 2; // is this needed?
     int jmax_plus_one = jmax + 1;
 
-    /* Left Wall */
-    for (j = 0; j <= jmax + 1; j++){
-        (*U)[j*imax_plus_two] = 0;       /// Possibly redundant - always remains zero. Can be removed?
-        (*V)[j*imax_plus_two] = -(*V)[j*imax_plus_two + 1];
+    for (j = 0; j <= jmax_plus_one; j++){
+        /* Left Wall */
+        U[0][j] = 0;        /// Possibly redundant - always remains zero. Can be removed?
+        V[0][j] = -V[1][j];
+
+        /* Right Wall */
+        U[imax][j] = 0;
+        V[imax_plus_one][j] = -V[imax][j];
     }
 
-    /* Right Wall */
-    for (j = 0; j <= jmax + 1; j++){
-        (*U)[j*imax_plus_two + imax] = 0;  /// Possibly redundant - always remains zero. Can be removed?
-        (*V)[j*imax_plus_two + imax_plus_one] = -(*V)[j*imax_plus_two + imax];
-    }
-
-    /* Bottom Wall */
     for (i = 0; i <= imax + 1; i++){
-        (*U)[i] = -(*U)[imax_plus_one+i];
-        (*V)[i] = 0;                       /// Possibly redundant - always remains zero. Can be removed?
-    }
+        /* Bottom Wall */
+        U[i][0] = -U[i+1][0];
+        V[i][0] = 0;        /// Possibly redundant - always remains zero. Can be removed?
 
-    /* Top (Moving) Wall */
-    for (i = 0; i <= imax + 1; i++){
-        (*U)[jmax_plus_one*imax_plus_two + i] = 2.0 - (*U)[jmax*imax_plus_two + i];   /// Corresponding to U_top = 1
-        (*V)[jmax*imax_plus_two + i] = 0;  /// Possibly redundant - always remains zero. Can be removed?
+        /* Top (Moving) Wall */
+        U[i][jmax_plus_one] = 2 - U[i][jmax];   /// Corresponding to U_top = 1
+        V[i][jmax] = 0;
     }
 }
